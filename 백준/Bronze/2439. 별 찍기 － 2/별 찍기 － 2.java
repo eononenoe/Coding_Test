e3c523922295c5d83dc8499bf81ef8df5799cbc0
eononenoe <jeonghyeon0921@naver.com>
2025-09-02 12:44:06 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 320 ms, Memory: 23200 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for(int i=1;i<=t;i++){
+			for(int j=t;j>i;j--){
+				System.out.print(" ");
+			}
+			for(int x=1;x<=i;x++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
