7fa5464752b60ac1ad263e8c41712218a5d8626f
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-09-05 13:04:41 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean[] submit = new boolean[31];
+		
+		for(int i=0;i<28;i++){
+			int n = sc.nextInt();
+			submit[n] = true;
+		}
+		for(int i=1;i<=30;i++){
+			if(!submit[i]){
+				System.out.println(i);
+			}
+		}
+	}
+}
