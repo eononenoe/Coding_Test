ade358a8e1c71feab4d43b4192a881bc3c4947c6
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-09-07 22:50:39 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 204 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int list[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			list[i] = i + 1;
+		}
+		for (int x = 0; x < m; x++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+
+			while (i < j) {
+				int temp = list[i - 1];
+				list[i - 1] = list[j - 1];
+				list[j - 1] = temp;
+				i++;
+				j--;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			System.out.print(list[i] + " ");
+		}
+
+	}
+}
