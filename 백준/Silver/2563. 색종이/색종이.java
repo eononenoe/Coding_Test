15b6186452272c708b0033f2a850003bde60a91b
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-09-19 15:43:02 +0900

    [Silver V] Title: 색종이, Time: 184 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int[][] paper = new int[100][100];
+
+        for(int i=0;i<t;i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            for(int j=x;j<x+10;j++){
+                for(int k=y;k<y+10;k++){
+                    paper[j][k]=1;
+                }
+            }
+        }
+        int a = 0;
+        for(int i=0;i<100;i++){
+            for(int j=0;j<100;j++){
+                if(paper[i][j]==1){
+                    a++;
+                }
+            }
+        }
+        System.out.println(a);
+    }
+}
