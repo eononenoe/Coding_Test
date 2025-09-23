b331160d0af44b42b75997ec4383b853120b32d2
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-09-23 17:02:12 +0900

    Update 숫자 카드.java

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Set<Integer> set = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             set.add(sc.nextInt());
@@ -22,4 +22,5 @@ public class Main {
         }
 
     }
+
 }
