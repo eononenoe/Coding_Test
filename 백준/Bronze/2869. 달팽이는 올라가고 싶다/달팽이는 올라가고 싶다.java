5b38042cd52e1dbc80cc964cf85942630d3b5faf
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-09-23 16:18:55 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 164 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int v = sc.nextInt();
+
+        int day = (v-b + (a-b)-1)/ (a-b);
+        System.out.println(day);
+    }
+}
