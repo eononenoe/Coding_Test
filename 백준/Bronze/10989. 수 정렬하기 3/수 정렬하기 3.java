78e0ffb18b77ea33dc0564746add0f7b5c01282e
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-11-05 20:09:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1656 ms, Memory: 300568 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+        for(int i=0;i<t;i++){
+           int num = Integer.parseInt(br.readLine());
+            count[num]++;
+        }
+
+        for(int i=1;i<=10000;i++){
+            while(count[i]>0){
+                bw.write(i+"\n");
+                count[i]--;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
