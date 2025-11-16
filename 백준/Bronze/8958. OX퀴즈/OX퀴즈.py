651bd90ee27a03f06b07206afcc8c65bc4af019a
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-11-16 23:42:39 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    score = 0     
+    total = 0    
+
+    for ch in s:
+        if ch == 'O':
+            score += 1
+            total += score
+        else:
+            score = 0
+    print(total)
