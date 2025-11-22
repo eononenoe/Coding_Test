3ffdca45a40f562acb44364bc42fe629f8cf280f
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-11-22 20:56:28 +0900

    [Bronze IV] Title: 평균 점수, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,17 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
+scores = [int(input()) for _ in range(5)]
 
-if(a<40):
-    a=40
-if(b<40):
-    b=40
-if(c<40):
-    c=40
-if(d<40):
-    d=40
-if(e<40):
-    e=40
-print((a+b+c+d+e)//5)
+for i in range(5):
+    if scores[i] < 40:
+        scores[i]=40
+
+print(sum(scores)//5)
