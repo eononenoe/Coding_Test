848104b83e37dbbcdd4245eb2c4bec608c54c900
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-11-23 14:04:57 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -4,10 +4,11 @@ blocks = list(map(int,input().split()))
 result = 0
 
 for i in range(1,w-1):
-    left_max = max(blocks[:i])
-    right_max = max(blocks[i+1:])
-    water = min(left_max, right_max) - blocks[i]
+    left = max(blocks[:i])
+    right = max(blocks[i+1:])
+    water = min(left,right)-blocks[i]
+  
     if water > 0:
         result += water
-        
+
 print(result)
