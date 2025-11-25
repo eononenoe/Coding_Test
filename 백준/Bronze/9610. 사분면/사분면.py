b4a49dffe55c0e23b44c68780fb1a0a73c0a177a
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-11-25 16:50:37 +0900

    [Bronze III] Title: 사분면, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,24 @@
+t = int(input())
+Q1 = 0
+Q2 = 0
+Q3 = 0
+Q4 = 0
+AXIS = 0
+for _ in range(t):
+    x,y = map(int,input().split())
+    if x > 0 and y > 0:
+        Q1 += 1
+    elif x>0 and y<0:
+        Q4 += 1
+    elif x<0 and y>0:
+        Q2 += 1
+    elif x<0 and y<0:
+        Q3 += 1
+    else:
+        AXIS += 1
+
+print(f"Q1: {Q1}")
+print(f"Q2: {Q2}")
+print(f"Q3: {Q3}")
+print(f"Q4: {Q4}")
+print(f"AXIS: {AXIS}")
