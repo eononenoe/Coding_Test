e126b02d4207efea44999b8e95e8a4096a0f1d10
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-12-14 23:35:53 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1,n+1):
+    space = 2 * (n - i)
+    print('*'*i+' '*space+'*'*i)
+
+for i in range(n-1,0,-1):
+    space = 2 * (n - i)
+    print('*'*i+' '*space+'*'*i)
