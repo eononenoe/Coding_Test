339c7fa72d57f18a102bf00d446077cf6f7fbad3
JeongHyeon Jeon <jeonghyeon0921@naver.com>
2025-12-21 16:19:09 +0900

    [Bronze III] Title: 점수계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int,input().split()))
+count = 0
+sum = 0
+for i in range(n):
+    if arr[i] == 1:
+        count += 1
+        sum += count
+    else:
+        count = 0
+print(sum)
